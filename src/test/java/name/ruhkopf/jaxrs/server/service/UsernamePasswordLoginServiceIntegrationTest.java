b56f4bcc466fe21aecbf6c860259f40a39b4da6f
Patrick Ruhkopf <patrickruhkopf@gmail.com>
2013-09-05 01:26:04 -0400

    fix tests

--- src/test/java/name/ruhkopf/jaxrs/server/service/UsernamePasswordLoginServiceIntegrationTest.java
+++ src/test/java/name/ruhkopf/jaxrs/server/service/UsernamePasswordLoginServiceIntegrationTest.java
@@ -1,13 +1,20 @@
 package name.ruhkopf.jaxrs.server.service;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 import java.util.UUID;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 import name.ruhkopf.jaxrs.server.model.LoginEntity;
+import name.ruhkopf.jaxrs.server.service.PersistenceService.Closure;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -20,6 +27,37 @@ public class UsernamePasswordLoginServiceIntegrationTest extends AbstractService
 
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
+
+	@Inject
+	private PersistenceService persistenceService;
+
+	@Before
+	public void checkDatabaseState()
+	{
+		assertThat(usernamePasswordLoginService.findAllLogins(0, 10), empty());
+		assertThat(usernamePasswordLoginService.countAllLogins(), is(0L));
+	}
+
+	/**
+	 * since this test is not transactional on purpose, stuff actually gets written to the db. let's be nice and cleanup
+	 * after
+	 */
+	@After
+	public void cleanup()
+	{
+		persistenceService.runInTransaction(new Closure()
+		{
+			@Override
+			public void invoke(EntityManager em)
+			{
+				for (LoginEntity login : usernamePasswordLoginService.findAllLogins(0, 10))
+				{
+					em.remove(login);
+				}
+			}
+		});
+
+	}
 
 	@Test
 	public void shouldNotCreateDuplicateUserToken()
